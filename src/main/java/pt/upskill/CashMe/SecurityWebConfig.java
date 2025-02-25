17d55f7aac7fc5c8602c8b85fee819177a8c7c7a
Gonçalo Cabral Lopes <gonca_lopes1996@hotmail.com>
2025-02-25 12:13:41 +0000

    security v2

--- src/main/java/pt/upskill/CashMe/SecurityWebConfig.java
+++ src/main/java/pt/upskill/CashMe/SecurityWebConfig.java
@@ -49,7 +49,7 @@ public class SecurityWebConfig {
             auth.requestMatchers("/login", "/favicon.ico", "/styles/**", "/scripts/**", "/images/**", "/home", "/signup", "/", "/privacy-policy","/redirectToMainPage", "/terms","/faq").permitAll();
             auth.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll();
             // Qualquer utilizador autenticado pode ver :
-            auth.requestMatchers("/categories", "/categories/**", "/cart", "/cart/**", "/mainPage", "/scan/**", "/storeView", "/storeView/**","/wishlist/**", "/aboutUs", "/contacts", "/messageSubmit", "/storesMap","/account","/item/**", "/", "/underConstruction","/api/stores","/stores").hasAnyRole("USER", "ADMIN");
+            auth.requestMatchers("/categories", "/categories/**", "/cart", "/cart/**", "/mainPage", "/scan/**", "/storeView", "/storeView/**","/wishlist/**", "/aboutUs", "/contacts", "/messageSubmit", "/storesMap","/account","/item/**", "/", "/underConstruction","/api/","/api/**").hasAnyRole("USER", "ADMIN");
             auth.requestMatchers("/**").denyAll();
         });
 
