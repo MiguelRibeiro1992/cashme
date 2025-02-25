20e225aa44751c770b4d234b49542088b30a4ad0
Gonçalo Cabral Lopes <gonca_lopes1996@hotmail.com>
2025-02-25 12:06:44 +0000

    Segurança.

--- src/main/java/pt/upskill/CashMe/SecurityWebConfig.java
+++ src/main/java/pt/upskill/CashMe/SecurityWebConfig.java
@@ -46,10 +46,10 @@ public class SecurityWebConfig {
         });
 
         httpSecurity.authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/login", "/favicon.ico", "/styles/**", "/scripts/**", "/images/**", "/home", "/signup", "/", "/privacy-policy","/redirectToMainPage", "/terms","/faq","/stores").permitAll();
+            auth.requestMatchers("/login", "/favicon.ico", "/styles/**", "/scripts/**", "/images/**", "/home", "/signup", "/", "/privacy-policy","/redirectToMainPage", "/terms","/faq").permitAll();
             auth.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll();
             // Qualquer utilizador autenticado pode ver :
-            auth.requestMatchers("/categories", "/categories/**", "/cart", "/cart/**", "/mainPage", "/scan/**", "/storeView", "/storeView/**","/wishlist/**", "/aboutUs", "/contacts", "/messageSubmit", "/storesMap","/account","/item/**", "/", "/underConstruction","/api/stores").hasAnyRole("USER", "ADMIN");
+            auth.requestMatchers("/categories", "/categories/**", "/cart", "/cart/**", "/mainPage", "/scan/**", "/storeView", "/storeView/**","/wishlist/**", "/aboutUs", "/contacts", "/messageSubmit", "/storesMap","/account","/item/**", "/", "/underConstruction","/api/stores","/stores").hasAnyRole("USER", "ADMIN");
             auth.requestMatchers("/**").denyAll();
         });
 
